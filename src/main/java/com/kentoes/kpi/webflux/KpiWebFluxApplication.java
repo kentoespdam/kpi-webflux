9d7932b96b36fb25456aa71306fbe14e58a12494
Kentoes PDAM <108379342+kentoespdam@users.noreply.github.com>
2023-08-24 07:59:57 +0700

    Base Framework

---
+++ src/main/java/com/kentoes/kpi/webflux/KpiWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.kentoes.kpi.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpiWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpiWebFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kentoes/kpi/webflux/KpiWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kentoes.kpi.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpiWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
